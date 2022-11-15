448157ab723f724146062f5081775919fc6cefd7
joepoehlmann <118295265+joepoehlmann@users.noreply.github.com>
2022-11-15 10:43:39 -0600

    Update AlmostMaximizeCalculation.swift

--- Rectangle/WindowCalculation/AlmostMaximizeCalculation.swift
+++ Rectangle/WindowCalculation/AlmostMaximizeCalculation.swift
@@ -16,7 +16,7 @@ class AlmostMaximizeCalculation: WindowCalculation {
     override init() {
         let defaultHeight = Defaults.almostMaximizeHeight.value
         almostMaximizeHeight = (defaultHeight <= 0 || defaultHeight > 1)
-            ? 0.9
+            ? 1
             : CGFloat(defaultHeight)
 
         let defaultWidth = Defaults.almostMaximizeWidth.value
